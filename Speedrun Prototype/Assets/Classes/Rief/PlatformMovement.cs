54e2ff81d153ae3372ab4181bbd9683312590738
Rief Haalboom <superrieff@hotmail.com>
2017-09-14 10:40:23 +0200

    basic platform done, abstract class begin.

---
+++ Speedrun Prototype/Assets/Classes/Rief/PlatformMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PlatformMovement : MonoBehaviour {
+    
+    public GameObject thisPlatform;
+    public bool rightDir;
+    public int moveSpeed;
+
+	void Start () {
+    }
+	
+	void Update () {
+        Moving ();
+	}
+
+    void Moving ()
+    {   
+        if(rightDir == true)
+        {
+            thisPlatform.transform.Translate (Vector3.right * moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            thisPlatform.transform.Translate (-Vector3.right * moveSpeed * Time.deltaTime);
+        }
+        if(thisPlatform.transform.position.x >= 2)
+        {
+            rightDir = false;
+        }
+        if(thisPlatform.transform.position.x <= -2)
+        {
+            rightDir = true;
+        }
+    }
+}
