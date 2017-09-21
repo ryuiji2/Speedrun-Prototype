cd58df475b8c8aa8f88d3427a881ce33968f4409
Rief Haalboom <superrieff@hotmail.com>
2017-09-21 11:09:36 +0200

    Virtual class platforms wohooooooo

--- Speedrun Prototype/Assets/Classes/Rief/BasicPlatform.cs
+++ Speedrun Prototype/Assets/Classes/Rief/BasicPlatform.cs
@@ -3,6 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BasicPlatform : PlatformMovement {
-    
 
+
+    public override void Start ()
+    {
+        base.Start ();
+    }
+    public void Update ()
+    {
+        Moving ();
+    }
+    public override void Moving ()
+    {
+        base.Moving ();
+    }
 }
